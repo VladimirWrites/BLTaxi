e9363ae9f71dc7ab6cf71d4578017409da67e3c0
Vladimir Jovanović <write@vladimirj.dev>
2020-03-23 18:26:05 +0100

    Add tests for AppInfoProvider

---
+++ app/src/test/java/com/vlad1m1r/bltaxi/AppInfoProviderTest.kt
@@ -0,0 +1,20 @@
+package com.vlad1m1r.bltaxi
+
+import com.google.common.truth.Truth.assertThat
+import com.vlad1m1r.bltaxi.about.di.AppInfoProvider
+import org.junit.Test
+
+class AppInfoProviderTest {
+
+    val appInfoProvider: AppInfoProvider = AppInfoProviderImpl()
+
+    @Test
+    fun getApplicationId() {
+        assertThat(appInfoProvider.getApplicationId()).isEqualTo(BuildConfig.APPLICATION_ID)
+    }
+
+    @Test
+    fun getVersionName() {
+        assertThat(appInfoProvider.getVersionName()).isEqualTo(BuildConfig.VERSION_NAME)
+    }
+}
