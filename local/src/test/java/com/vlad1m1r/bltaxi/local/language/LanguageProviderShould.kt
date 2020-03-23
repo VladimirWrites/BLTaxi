0abad9c1abf869e0ff976861bad1c91188537ac4
Vladimir Jovanović <write@vladimirj.dev>
2020-03-23 22:04:38 +0100

    Add tests for LanguageProvider

---
+++ local/src/test/java/com/vlad1m1r/bltaxi/local/language/LanguageProviderShould.kt
@@ -0,0 +1,55 @@
+package com.vlad1m1r.bltaxi.local.language
+
+import android.content.Context
+import android.content.res.Configuration
+import android.content.res.Resources
+import android.os.Build
+import android.os.LocaleList
+import com.google.common.truth.Truth.assertThat
+import com.nhaarman.mockitokotlin2.mock
+import com.vlad1m1r.bltaxi.domain.Language
+import org.junit.Test
+import java.lang.reflect.Field
+import java.lang.reflect.Modifier
+
+import java.util.*
+
+class LanguageProviderShould {
+    val locale = mock<Locale> {
+        on { language }.thenReturn("en")
+    }
+    val locales = mock<LocaleList> {
+        on { get(0) }.thenReturn(locale)
+    }
+    val configuration = mock<Configuration> {
+        on { locales }.thenReturn(locales)
+    }
+    val resources = mock<Resources> {
+        on { configuration }.thenReturn(configuration)
+    }
+    val context = mock<Context> {
+        on { resources }.thenReturn(resources)
+    }
+
+    val languageProvider: LanguageProvider = LanguageProviderImpl(context)
+
+    @Test
+    fun getLanguage() {
+        setFinalStatic(Build.VERSION::class.java.getField("SDK_INT"), Build.VERSION_CODES.N)
+
+        val language = languageProvider.getLanguage()
+
+        assertThat(language).isEqualTo(Language.EN)
+    }
+
+    @Throws(Exception::class)
+    fun setFinalStatic(field: Field, newValue: Any) {
+        field.isAccessible = true
+
+        val modifiersField = Field::class.java.getDeclaredField("modifiers")
+        modifiersField.isAccessible = true
+        modifiersField.setInt(field, field.modifiers and Modifier.FINAL.inv())
+
+        field.set(null, newValue)
+    }
+}
