54af7375d35fd30b85b4f9ee52adfba63ebf99f4
Vladimir Jovanović <write@vladimirj.dev>
2020-03-24 08:58:41 +0100

    Add tests for changing theme

--- settings/src/test/java/com/vlad1m1r/bltaxi/settings/SettingsViewModelShould.kt
+++ settings/src/test/java/com/vlad1m1r/bltaxi/settings/SettingsViewModelShould.kt
@@ -1,5 +1,7 @@
 package com.vlad1m1r.bltaxi.settings
 
+import androidx.appcompat.app.AppCompatDelegate.*
+import com.google.common.truth.Truth.assertThat
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
 import com.vlad1m1r.basedata.StringResolver
@@ -9,7 +11,11 @@ import org.junit.Test
 
 class SettingsViewModelShould {
 
-    private val stringResolver = mock<StringResolver>()
+    private val stringResolver = mock<StringResolver> {
+        on { getString(R.string.theme_value_dark) }.thenReturn("dark")
+        on { getString(R.string.theme_value_light) }.thenReturn("light")
+        on { getString(R.string.theme_value_default) }.thenReturn("default")
+    }
     private val crashReport = mock<CrashReport>()
     private val tracker = mock<Tracker>()
 
@@ -40,6 +46,20 @@ class SettingsViewModelShould {
     }
 
     @Test
-    fun changeTheme() {
+    fun changeTheme_toDark() {
+        settingsViewModel.changeTheme("dark")
+        assertThat(settingsViewModel.mode.get()).isEqualTo(MODE_NIGHT_YES)
+    }
+
+    @Test
+    fun changeTheme_toLight() {
+        settingsViewModel.changeTheme("light")
+        assertThat(settingsViewModel.mode.get()).isEqualTo(MODE_NIGHT_NO)
+    }
+
+    @Test
+    fun changeTheme_toDefault() {
+        settingsViewModel.changeTheme("default")
+        assertThat(settingsViewModel.mode.get()).isEqualTo(MODE_NIGHT_FOLLOW_SYSTEM)
     }
 }
