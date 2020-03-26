170d309d0298d39d930e3b32d18e5d50f9bf7041
Vladimir Jovanović <write@vladimirj.dev>
2020-03-26 21:15:30 +0100

    Add tests for date converters

--- local/src/test/java/com/vlad1m1r/bltaxi/local/database/ConvertersShould.kt
+++ local/src/test/java/com/vlad1m1r/bltaxi/local/database/ConvertersShould.kt
@@ -3,6 +3,7 @@ package com.vlad1m1r.bltaxi.local.database
 import com.google.common.truth.Truth.assertThat
 import com.vlad1m1r.bltaxi.domain.Language
 import org.junit.Test
+import java.util.*
 
 class ConvertersShould {
 
@@ -16,5 +17,28 @@ class ConvertersShould {
     @Test
     fun convertLanguageToLanguageCode() {
         assertThat(converters.languageToLanguageCode(Language.BS)).isEqualTo("bs")
+    }
+
+    @Test
+    fun convertTimestampToDate() {
+        val calendar = Calendar.getInstance()
+        assertThat(converters.toDate(calendar.timeInMillis)).isEqualTo(calendar.time)
+    }
+
+    @Test
+    fun convertNullTimestampToNullDate() {
+        assertThat(converters.toDate(null)).isNull()
+    }
+
+    @Test
+    fun convertDateToTimestamp() {
+        val calendar = Calendar.getInstance()
+        assertThat(converters.toTimestamp(calendar.time)).isEqualTo(calendar.timeInMillis)
+    }
+
+    @Test
+    fun convertNullDateToNullTimestamp() {
+        val calendar = Calendar.getInstance()
+        assertThat(converters.toTimestamp(null)).isNull()
     }
 }
