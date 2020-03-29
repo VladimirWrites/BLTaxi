19224736b61166bc85d6ef1aa52df67eb03790c4
Vladimir Jovanović <write@vladimirj.dev>
2020-03-29 17:08:01 +0200

    Add test for CrashReport

---
+++ analytics/src/test/java/com/vlad1m1r/bltaxi/analytics/CrashReportShould.kt
@@ -0,0 +1,30 @@
+package com.vlad1m1r.bltaxi.analytics
+
+import android.app.Activity
+import android.os.Build
+import androidx.test.platform.app.InstrumentationRegistry
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [Build.VERSION_CODES.P])
+class CrashReportShould {
+
+    val context = InstrumentationRegistry.getInstrumentation().targetContext
+
+    val sharedPreferences = context.getSharedPreferences(context.packageName, Activity.MODE_PRIVATE);
+    val crashReport: CrashReport = CrashReportImpl(context, sharedPreferences)
+
+    @Test
+    fun initialize_whenNotInitialized() {
+        val keyCrashReport = context.getString(R.string.pref_key_crash_reports)
+        sharedPreferences.edit().clear()
+
+        crashReport.initialize()
+
+        assertThat(sharedPreferences.getBoolean(keyCrashReport, false)).isTrue()
+    }
+}
